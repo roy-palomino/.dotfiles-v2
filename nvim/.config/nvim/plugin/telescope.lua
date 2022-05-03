1e0bf243d507950f20aef23c85539adf1fd4bd50
Roy Palomino <roy100995@gmail.com>
2022-05-02 23:22:02 -0500

    refactor: format and change telescope style

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -1,24 +1,24 @@
-local tele_themes = require'telescope.themes'
+-- local tele_themes = require'telescope.themes'
 
--- Base opts for all telescope.buildin
-local function base_opts(opts)
-  local _opts = tele_themes.get_dropdown {
-    winblend = 25,
-    border = true,
-    previewer = false,
-  }
-  return vim.tbl_deep_extend("force", _opts, opts)
-end
+-- -- Base opts for all telescope.buildin
+-- local function base_opts(opts)
+--   local _opts = tele_themes.get_dropdown {
+--     winblend = 10,
+--     border = true,
+--     previewer = false,
+--   }
+--   return vim.tbl_deep_extend("force", _opts, opts)
+-- end
 
-local gf_opts = base_opts {}
+-- local gf_opts = base_opts {}
 
-local ff_opts = base_opts {
-  find_command = { "rg", "--ignore", "--hidden", "--files", "--unrestricted" },
-}
+-- local ff_opts = base_opts {
+--   find_command = { "rg", "--ignore", "--hidden", "--files", "--unrestricted" },
+-- }
 
-require('telescope').setup {
-  pickers = {
-    git_files = gf_opts,
-    find_files = ff_opts,
-  }
-}
+-- require('telescope').setup {
+--   pickers = {
+--     git_files = gf_opts,
+--     find_files = ff_opts,
+--   }
+-- }
