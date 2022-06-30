34c4637566e387fb4b715a531b029169d96bca9e
Roy Palomino <roy100995@gmail.com>
2022-06-30 18:51:43 -0500

    feat: add mappings for lsp interaction

--- nvim/.config/nvim/plugin/keymaps.lua
+++ nvim/.config/nvim/plugin/keymaps.lua
@@ -49,6 +49,20 @@ nmap { '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>',
 nmap { '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', silent }
 nmap { '<leader>fm', '<cmd>lua require(\'telescope.builtin\').keymaps()<cr>', silent }
 
+-- LSP mapping
+nmap { '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', silent }
+nmap { '<leader>gD', ':lua vim.lsp.buf.declaration()<CR>', silent }
+nmap { '<leader>gi', ':lua vim.lsp.buf.implementation()<CR>', silent }
+nmap { '<leader>gr', ':lua vim.lsp.buf.references()<CR>', silent }
+nmap { '<leader>sh', ':lua vim.lsp.buf.signature_help()<CR>', silent }
+nmap { '<leader>rn', ':lua vim.lsp.buf.rename()<CR>', silent }
+nmap { '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', silent }
+nmap { '<leader>sd', '<cmd>lua vim.diagnostic.open_float()<CR>', silent }
+nmap { '<leader>[d', ':lua vim.lsp.diagnostic.goto_prev()<CR>', silent }
+nmap { '<leader>]d', ':lua vim.lsp.diagnostic.goto_next()<CR>', silent }
+nmap { 'F', ':lua vim.lsp.buf.formatting()<CR>', { noremap = true } }
+nmap { 'K', ':lua vim.lsp.buf.hover()<CR>', silent }
+
 -- Neogit mapping
 nmap { '<C-g>', '<cmd>lua require("neogit").open({ kind = "split_above" })<cr>', silent }
 
