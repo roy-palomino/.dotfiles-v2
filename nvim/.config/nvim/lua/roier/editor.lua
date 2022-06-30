48e3944fda9ddaf9ed3b387cc55e63b25506cd36
Roy Palomino <roy100995@gmail.com>
2022-06-30 18:15:07 -0500

    refactor: add blank line indentation config

--- nvim/.config/nvim/lua/roier/editor.lua
+++ nvim/.config/nvim/lua/roier/editor.lua
@@ -1,6 +1,12 @@
 -- Icons setup
 local dev_icons = require'nvim-web-devicons'
+local indent_blankline = require'indent_blankline'
 
 dev_icons.setup {
   default = true
+}
+
+indent_blankline.setup {
+  space_char_blankline = " ",
+  show_current_context = true,
 }
