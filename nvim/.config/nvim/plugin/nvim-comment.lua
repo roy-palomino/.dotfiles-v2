6fa38452a95c9585376a3019adbf689a991152cb
Roy Palomino <roy100995@gmail.com>
2022-07-30 22:55:40 -0500

    refactor: add support to comment in tsx files

--- nvim/.config/nvim/plugin/nvim-comment.lua
+++ nvim/.config/nvim/plugin/nvim-comment.lua
@@ -1,5 +1,11 @@
 local nvim_comment = require("nvim_comment")
 
+require'nvim-treesitter.configs'.setup {
+  context_commentstring = {
+    enable = true
+  }
+}
+
 nvim_comment.setup {
   context_commentstring = { enable = true },
   marker_padding = true,
@@ -7,6 +13,8 @@ nvim_comment.setup {
   create_mappings = true,
   hook = function()
     if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
+      require("ts_context_commentstring.internal").update_commentstring()
+    elseif vim.api.nvim_buf_get_option(0, "filetype") == "typescriptreact" then
       require("ts_context_commentstring.internal").update_commentstring()
     end
   end
