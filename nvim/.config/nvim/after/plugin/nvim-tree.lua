d55ea9ff7287a4c57aff0700ec4070744f5b8c8a
Roy Palomino <roy100995@gmail.com>
2022-09-05 12:09:16 -0500

    feat: add custom config file for nvim-tree

---
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,47 @@
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  open_on_tab         = false,
+  hijack_cursor       = true,
+  sync_root_with_cwd  = false,
+  hijack_directories  = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  filters = {
+    dotfiles = false,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = "-", action = "dir_up" },
+      }
+    }
+  },
+  actions = {
+    open_file = {
+      resize_window = false
+    }
+  }
+}
