5c4d975c2726b63318a9df14a929ab33eeb404c4
Roy Palomino <roy100995@gmail.com>
2022-09-05 12:31:40 -0500

    refactor: add packages for file tree and status line

--- nvim/.config/nvim/lua/roier/packer.lua
+++ nvim/.config/nvim/lua/roier/packer.lua
@@ -1,75 +1,71 @@
 return require('packer').startup(function(use)
 
-  -- LSP config
-  use 'onsails/lspkind.nvim'
+    -- LSP config
+    use 'onsails/lspkind.nvim'
 
-  use {
-    'williamboman/nvim-lsp-installer',
-  }
+    use {'williamboman/nvim-lsp-installer'}
 
-  -- Nvim-cmp packages
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+    -- Nvim-cmp packages
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
 
-  -- Snippets packages
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'rafamadriz/friendly-snippets'
+    -- Snippets packages
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'rafamadriz/friendly-snippets'
 
-  use 'tjdevries/colorbuddy.nvim'
+    use 'tjdevries/colorbuddy.nvim'
 
-  use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/plenary.nvim'
 
-  use {
-    'TimUntersberger/neogit',
-    requires = 'nvim-lua/plenary.nvim'
-  }
+    use {'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim'}
 
-  use({
-    'catppuccin/nvim',
-    as = 'catppuccin'
-  })
+    use({'catppuccin/nvim', as = 'catppuccin'})
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      {'nvim-lua/plenary.nvim'}
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}}
     }
-  }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'nvim-treesitter/playground'
+    -- TREE NAVIGATION
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+            'kyazdani42/nvim-web-devicons' -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
 
-  use 'kyazdani42/nvim-web-devicons'
+    use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
 
-  -- Comment packages
-  use 'terrortylor/nvim-comment'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'nvim-treesitter/playground'
 
-  use 'lukas-reineke/indent-blankline.nvim'
+    use 'kyazdani42/nvim-web-devicons'
 
-  -- Dart & Flutter support
-  -- use 'thosakwe/vim-flutter'
-  use 'dart-lang/dart-vim-plugin'
-  use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
+    -- Comment packages
+    use 'terrortylor/nvim-comment'
+    use 'JoosepAlviste/nvim-ts-context-commentstring'
 
-  -- Formatting
-  use 'sbdchd/neoformat'
+    use 'lukas-reineke/indent-blankline.nvim'
 
-  -- Completion for lua
-  use 'tjdevries/nlua.nvim'
-  use 'nvim-lua/completion-nvim'
+    -- Dart & Flutter support
+    -- use 'thosakwe/vim-flutter'
+    use 'dart-lang/dart-vim-plugin'
+    use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
 
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
+    -- Formatting
+    use 'sbdchd/neoformat'
 
+    -- Completion for lua
+    use 'tjdevries/nlua.nvim'
+    use 'nvim-lua/completion-nvim'
+
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
 
 end)
