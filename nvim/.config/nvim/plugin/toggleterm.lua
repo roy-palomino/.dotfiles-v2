4147abba2e36b2140670cbb21dd9ded1b38bc1a6
Roy Palomino <roy100995@gmail.com>
2022-12-03 13:48:05 -0500

    refactor: toggle term starts in insert mode

--- nvim/.config/nvim/plugin/toggleterm.lua
+++ nvim/.config/nvim/plugin/toggleterm.lua
@@ -1,7 +1,8 @@
 require('toggleterm').setup {
   on_open = function(term)
+    -- Map <Esc> to return to normal mode in terminal buffers
     -- vim.cmd("startinsert!")
-    -- vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<C-\\><C-h>", { noremap = true })
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<esc>", "<C-\\><C-n>", { noremap = true })
 
     -- Send <Esc> when toggling term
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'm', true)
